2bc35f50929cc9e9a0cffd2674070b79a51d3121
Juan Soler <juan.soler@perficient.com>
2025-07-10 17:53:56 -0300

    Add iframe for game embed from itch.io

--- assets/components/MainPage.tsx
+++ assets/components/MainPage.tsx
@@ -6,28 +6,16 @@ import GameBar from "@/assets/components/GameBar";
 import Footer from "@/assets/components/Footer";
 import GameDescription from "@/assets/components/GameDescription";
 import OtherGames from "@/assets/components/OtherGames";
-import {Unity, useUnityContext} from "react-unity-webgl";
-import {Fragment} from "react";
 
 const Container = () => {
-    const { unityProvider, loadingProgression, isLoaded } = useUnityContext({
-        loaderUrl: '../unity/Build/UnityLoader.js',
-        dataUrl: "../unity/Build/Unity.data",
-        frameworkUrl: "../unity/Build/Unity.framework.js",
-        codeUrl: "../unity/Build/Unity.wasm",
-    });
+
     return (
         <View style={styles.container}>
             <View style={styles.view}>
                 <NavBar />
                 <GameBar />
                 <BackgroundDragon />
-                <Fragment>
-                    {!isLoaded && (
-                        <p>Loading Application... {Math.round(loadingProgression * 100)}%</p>
-                    )}
-                <Unity unityProvider={unityProvider} style={styles.gameIconLayout}  />
-                </Fragment>
+                <iframe style={styles.gameIconLayout} src="https://itch.io/embed-upload/14057874?color=333333" width="1237" height="600"><a href="https://sparklmonkey.itch.io/element-revival">Play Element Revival on itch.io</a></iframe>
                 <GameDescription />
                 <OtherGames />
                 <Footer />
@@ -40,8 +28,8 @@ const styles = StyleSheet.create({
     gameIconLayout: {
         position: 'absolute',
         top: 300,
-        width: 1072,
-        height: 519,
+        width: 1237,
+        height: 600,
         overflow: "hidden"
     },
     view: {
